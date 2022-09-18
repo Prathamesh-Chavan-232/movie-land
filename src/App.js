b7e9c6bfd2406c2712efd04930105c886124e9f1
falcon_codes <86522183+Prathamesh-Chavan-232@users.noreply.github.com>
2022-09-18 17:16:25 +0530

    added api key

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useEffect } from "react";
+const API_URL = "http://www.omdbapi.com/?apikey=593ef835";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Movie land</h1>
     </div>
   );
 }

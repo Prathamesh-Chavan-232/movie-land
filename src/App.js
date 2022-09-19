594547e5a2f4c51a7e798833b2930300b9f1e4e5
falcon_codes <86522183+Prathamesh-Chavan-232@users.noreply.github.com>
2022-09-19 16:21:23 +0530

    collecting movie data from api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
 import { useEffect } from "react";
-const API_URL = "http://www.omdbapi.com/?apikey=593ef835";
+const API_URL = "http://www.omdbapi.com/?apikey=a87458c7";
 function App() {
+  const searchMovies = async (title) => {
+    const res = await fetch(`${API_URL}&s=${title}`);
+    const data = await res.json();
+    console.log(data.Search);
+  };
+  // import the data from the api
+  useEffect(() => {
+    searchMovies("Superman");
+    return () => {};
+  }, []);
+
   return (
     <div className="App">
       <h1>Movie land</h1>

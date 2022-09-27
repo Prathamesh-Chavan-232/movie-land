a5dbeb5c9dbbf6f52722e81d42556863014b8c48
Prathamesh Chavan <prathamesh.chavanpsc2018@gmail.com>
2022-09-28 03:41:51 +0530

    popular movies below carousel

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,6 +1,6 @@
 import { apilinks } from "../api/apiconfig";
 import Card from "./Card";
-import React, { useEffect, useState, useCallback } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 
@@ -8,26 +8,31 @@ export const MovieList = () => {
   const { type } = useParams();
   const [Movies, setMovies] = useState([]);
 
-  const fetchMovies = useCallback(async () => {
-    let res;
-    if (type === "upcoming") {
-      res = await fetch(`${apilinks.upcomingMovies}`);
-    } else if (type === "top-rated") {
-      res = await fetch(`${apilinks.topRatedMovies}`);
-    } else {
-      res = await fetch(`${apilinks.popularMovies}`);
-    }
-    const data = await res.json();
-    console.log(data.results);
-    setMovies(data.results);
-  });
+  // const fetchMovies = async () => {
+  //   const res = await fetch(`${apilinks.topRatedMovies}`);
+  //   const data = await res.json();
+  //   console.log(data.results);
+  //   setMovies(data.results);
+  // };
+
   // Fetch Movies Data from the api
   useEffect(() => {
+    const fetchMovies = async () => {
+      let res;
+      if (type === "upcoming") {
+        res = await fetch(`${apilinks.upcomingMovies}`);
+      } else if (type === "top-rated") {
+        res = await fetch(`${apilinks.topRatedMovies}`);
+      } else {
+        res = await fetch(`${apilinks.popularMovies}`);
+      }
+      const data = await res.json();
+      console.log(data.results);
+      setMovies(data.results);
+    };
     fetchMovies();
-  }, [fetchMovies]);
-  useEffect(() => {
-    fetchMovies();
-  }, [fetchMovies, type]);
+  }, [type]);
+
   return (
     <div className="movie-list">
       <h2 className="movie-list-title">
